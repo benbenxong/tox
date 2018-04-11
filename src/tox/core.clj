0291781b7dede112d62bb16187e02e81436be7a1
alanzhang <hj_chang@163.com>
2018-04-11 17:30:17 +0800

    fix parser.20180411

--- src/tox/core.clj
+++ src/tox/core.clj
@@ -13,17 +13,27 @@
 
 ;;(time (j/query db ["select sysdate from dual"]))
 
+(def parser
+            (insta/parser "S = <'('> members <')'>
+                           <members> = member (<','> members)*
+                           <member> = A | AB
+                           A = #'[^\\(\\)\\,]+'
+                           B = <'('> A <','> A <')'>
+                           AB = A B"))
+
 (def cli-options
   ;; An option with a required argument
-  [["-p" "--port PORT" "Port number"
-    :default 80
-    :parse-fn #(Integer/parseInt %)
-    :validate [#(< 0 % 0x10000) "Must be a number between 0 and 65536"]]
-   ;; A non-idempotent option
-   ["-v" nil "Verbosity level"
-    :id :verbosity
-    :default 0
-    :assoc-fn (fn [m k _] (update-in m [k] inc))]
+  [
+   ;; ["-p" "--port PORT" "Port number"
+   ;;  :default 80
+   ;;  :parse-fn #(Integer/parseInt %)
+   ;;  :validate [#(< 0 % 0x10000) "Must be a number between 0 and 65536"]]
+   ;; ;; A non-idempotent option
+   ;; ["-v" nil "Verbosity level"
+   ;;  :id :verbosity
+   ;;  :default 0
+   ;;  :assoc-fn (fn [m k _] (update-in m [k] inc))
+   ;;  ]
    ;; tox parameters
    ["-i" "--ifile filename" "input excel file"
     :validate [#(re-find #"\.xls" %) "Input excel file must be .xls|.xlsx file."]]
@@ -33,22 +43,11 @@
     :validate [#(re-find #"\.sql" %) "Sql script file must be .sql file."]]
    ["-w" "--sheet workstheets" "worksheet names. exp:(sheet1,sheet2) or (sheet1(loc1,loc2),sheet1(loc3,loc4))"
     :parse-fn (fn [s] (->> (parser s)
-                    (insta/transform {:S vector :A str :B hash-map :AB hash-map})))
-   ;; :validate [#(not (vector? (first %))) (str %) ]
+                           (insta/transform {:S vector :A str :B hash-map :AB hash-map})))
+    :validate [#(not (vector? (first %))) "sheet must be (s1,s2(a1,a2)..)" ]
    ]
    ;; A boolean option defaulting to nil
    ["-h" "--help"]])
-
-(def parser
-            (insta/parser "S = <'('> members <')'>
-                           <members> = member (<','> members)*
-                           <member> = A | AB
-                           A = #'[^\\(\\)\\,]+'
-                           B = <'('> A <','> A <')'>
-                           AB = A B"))
-
-
-
 
 (defn -main
   [& args]
